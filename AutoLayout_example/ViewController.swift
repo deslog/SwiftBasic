baa99487c59159a084b6fb540b5ca6b0e5aa47e8
Jisoo <dlwltn815@gmail.com>
2022-01-28 13:50:26 +0900

    Create colorView Outlet 변수, Create tapChangeColorButton Action 생성

--- AutoLayout_example/ViewController.swift
+++ AutoLayout_example/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorView: UIView!
+    // IBOutlet: 이 변수를 통해 스토리보드에서 만든 뷰를 코드를 통해 마음껏 변경 가능하다.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    // button을 클릭하면 액션을 취하게 만들기 위해 IBAction함수를 정의한다.
+    @IBAction func tapChangeColorButton(_ sender: UIButton) {
+        self.colorView.backgroundColor = UIColor.blue
+        print("색상 변경 버튼이 클릭되었습니다.")
+        // 콘솔창에 프린트됨.
+    }
+    
 }
 
